87408d4e354660366bad753edbbeb3fdffdd9e7d
rodionfisenko <minsk@joingo.com>
2021-07-13 16:42:42 +0300

    Add ProductOfNumbersService

--- src/main/java/ProductOfNumbersService.java
+++ src/main/java/ProductOfNumbersService.java
@@ -6,14 +6,14 @@ public class ProductOfNumbersService {
 
     public static void main(String[] args) {
 
-        int array[] = {-20, -20, 7, 20, 20};
+        int array[] = {-10, 8, 4, 2, 1, 1, -10};
         Arrays.sort(array);
         int length = array.length;
         try {
             int temp = max(array[0] * array[1] * array[length - 1], array[length - 1] * array[length - 2] * array[length - 3]);
             System.out.println(temp);
         } catch (IndexOutOfBoundsException ex) {
-            System.out.println("Необходимо ввести 3 и более числа");
+            System.out.println("Необходимо ввести не менее 3 чисел!");
         }
 
     }
