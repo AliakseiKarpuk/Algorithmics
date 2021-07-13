3581e052e6216b2af8b9a9c990dd2e37bd0981b3
rodionfisenko <minsk@joingo.com>
2021-07-13 16:27:05 +0300

    Add ProductOfNumbersService

--- src/main/java/IsCharsUniqueService.java
+++ src/main/java/IsCharsUniqueService.java
@@ -2,14 +2,14 @@ import java.util.Locale;
 
 public class IsCharsUniqueService {
 
-    public static boolean text(String text){
+    public static boolean text(String text) {
         text = text.toLowerCase(Locale.ROOT);
-        for(int i = 0; i < text.length(); i++){
-           for(int j = 0; j < text.length(); j++){
-               if(text.charAt(i) == text.charAt(j) && i != j){
-                   return false;
-               }
-           }
+        for (int i = 0; i < text.length(); i++) {
+            for (int j = 0; j < text.length(); j++) {
+                if (text.charAt(i) == text.charAt(j) && i != j) {
+                    return false;
+                }
+            }
         }
         return true;
     }
--- src/main/java/ProductOfNumbersService.java
+++ src/main/java/ProductOfNumbersService.java
@@ -1,2 +1,20 @@
+import java.util.Arrays;
+
+import static java.lang.Math.max;
+
 public class ProductOfNumbersService {
+
+    public static void main(String[] args) {
+
+        int array[] = {-20, -20, 7, 20, 20};
+        Arrays.sort(array);
+        int length = array.length;
+        try {
+            int temp = max(array[0] * array[1] * array[length - 1], array[length - 1] * array[length - 2] * array[length - 3]);
+            System.out.println(temp);
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Необходимо ввести 3 и более числа");
+        }
+
+    }
 }
