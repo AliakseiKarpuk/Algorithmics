1bb743f6c721d4e3244b0b5e4bd72b897422e8b5
rodionfisenko <minsk@joingo.com>
2021-07-15 09:05:41 +0300

    add matrix

--- src/main/java/Matrix/Matrix.java
+++ src/main/java/Matrix/Matrix.java
@@ -6,7 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 public class Matrix {
 
     public static void main(String[] args) {
-
         log.info(MatrixService.readFromFile());
 
         MatrixService.matrixWriter();
