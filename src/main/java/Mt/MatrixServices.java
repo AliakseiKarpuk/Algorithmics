39b7fa0f4923aa23c6f0a695d821f493395045f9
rodionfisenko <minsk@joingo.com>
2021-07-18 22:18:05 +0300

    add matrix

--- src/main/java/Mt/MatrixServices.java
+++ src/main/java/Mt/MatrixServices.java
@@ -10,85 +10,100 @@ public class MatrixServices {
     Integer[][] readArray;
     Matrix matrixArray;
 
-    String fileRead = "src/main/java/Mt/MatrixBefore";
-    String fileWrite = "src/main/java/Mt/MatrixAfter";
+    String fileRead = "src/main/java/Mt/MatrixBefore.txt";
+    String fileWrite = "src/main/java/Mt/MatrixAfter.txt";
 
+    public boolean runner() {
 
-    public boolean writeToFile() {
-        PrintWriter writer = null;
-        try {
-            writer = new PrintWriter(new FileWriter(fileWrite));
-            writer.print(newMatrixArray());
+        StringBuilder text = new StringBuilder();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileRead));
+             PrintWriter writer = new PrintWriter(new FileWriter(fileWrite));
+             PrintWriter newWriter = new PrintWriter(new FileWriter(fileRead))) {
+
+            createMatrix(array);
+            writeToFile(array, newWriter);
+            readFromFile(text, reader);
+
+            String line1 = text.toString();
+            String[] string = line1.trim().split("\n");
+            String[] length = string[0].trim().split(" ");
+            int lengthColumn = Integer.parseInt(length[0]);
+            int lengthRow = Integer.parseInt(length[0]);
+
+            readArray = convertFromStringToArray(string, lengthColumn, lengthRow);
+            matrixArray = new Matrix(readArray, lengthColumn, lengthRow);
+            writer.print(matrixArray);
             return true;
         } catch (IOException e) {
-            System.out.println("Не найден файл");
-            ;
+            return false;
         }
-        return false;
     }
 
-    public Matrix newMatrixArray() {
-        String line1 = readFromFile().toString();
-        String[] row = line1.trim().split("\n");
-        String[] length = row[0].trim().split(" ");
-        int lengthColumn = Integer.parseInt(length[0]);
-        int lengthRow = Integer.parseInt(length[0]);
-        readArray = new Integer[lengthColumn][lengthRow];
-
-        for (int i = 0; i < row.length - 1; i++) {
-            row[i] = row[i + 1];
+    public static boolean readFromFile(StringBuilder text, BufferedReader reader) {
+        try {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                text.append(line).append("\n");
+            }
+            return true;
+        } catch (Exception e) {
+            return false;
         }
-        String[] stringArrayColumn = Arrays.copyOf(row, row.length - 1);
+    }
+
+    public static boolean writeToFile(Integer[][] array, PrintWriter writer1) {
+        try {
+            writer1.print(printStringMatrix(array));
+            writer1.flush();
+            writer1.close();
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public static Integer[][] convertFromStringToArray(String[] text, int lengthRow, int lengthColumn) {
+        Integer[][] readArray;
+        readArray = new Integer[lengthRow][lengthColumn];
+
+        for (int i = 0; i < text.length - 1; i++) {
+            text[i] = text[i + 1];
+        }
+        String[] stringArrayColumn = Arrays.copyOf(text, text.length - 1);
         for (int i = 0; i < stringArrayColumn.length; i++) {
             String[] stringArrayRow = stringArrayColumn[i].trim().split(" ");
             for (int j = 0; j < stringArrayRow.length; j++) {
                 readArray[i][j] = Integer.parseInt(stringArrayRow[j]);
             }
         }
-        return matrixArray = new Matrix(readArray, lengthColumn, lengthRow);
+        return readArray;
     }
 
-    public StringBuilder readFromFile() {
-
-        StringBuilder text = new StringBuilder();
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileRead));
-             PrintWriter writer = new PrintWriter(new FileWriter(fileRead))) {
-
-            createMatrix(array);
-            writer.print(printStringMatrix(array));
-            writer.flush();
-            writer.close();
-
-            String line;
-            while ((line = reader.readLine()) != null) {
-                text.append(line).append("\n");
-            }
-            return text;
-        } catch (IOException e) {
-            return null;
-        }
-    }
-
-    private static Integer[][] createMatrix(Integer[][] array) {
+    public static Integer[][] createMatrix(Integer[][] array) {
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array[0].length; j++) {
-                int temp = (int) (Math.random() * 9);
-                array[i][j] = temp % 3;
+            for (int j = 0; j < array[i].length; j++) {
+                    int temp = (int) (Math.random() * 2);
+                    array[i][j] = temp % 3;
             }
             int count = 1;
-            for (int j = 0; j < array[0].length; j++) {
+            for (int j = 0; j < array[i].length; j++) {
                 if (array[i][j] == 0) {
-                    if (count > 1) {
-                        array[i][j] = (int) (Math.random() * 9) - 9;
+                        if (count > 1) {
+                            int number;
+                            do {
+                                number = (int) (Math.random() * 14) - 5;
+                            } while (number == 0);
+                            array[i][j] = number;
+                        }
+                        count++;
                     }
-                    count++;
                 }
             }
-        }
         return array;
     }
 
-    private static String printStringMatrix(Integer[][] array) {
+    public static String printStringMatrix(Integer[][] array) {
         StringBuffer string = new StringBuffer();
         string.append(array.length).append(" ").append(array[0].length).append("\n");
         for (int i = 0; i < array.length; i++) {
--- src/test/java/MatrixTest/matrixTest.java
+++ src/test/java/MatrixTest/matrixTest.java
@@ -1,26 +1,49 @@
 package MatrixTest;
 
 import Mt.MatrixServices;
+import lombok.SneakyThrows;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
 public class matrixTest {
 
+    String fileRead = "src/main/java/Mt/MatrixBefore.txt";
+    int size = (int) (Math.random() * 8) + 2;
+    Integer[][] array = new Integer[size][size];
+
     @Test
-    public void writeToFileTest() {
+    public void workTest(){
         MatrixServices mt = new MatrixServices();
-        Assert.assertTrue(mt.writeToFile());
+        Assert.assertTrue(mt.runner());
+    }
+
+    @SneakyThrows
+    @Test
+    public void readFromFileTest(){
+        BufferedReader reader = new BufferedReader(new FileReader(fileRead));
+        StringBuilder text = new StringBuilder();
+        Assert.assertTrue(MatrixServices.readFromFile(text, reader));
+    }
+
+    @SneakyThrows
+    @Test
+    public void writeToFileTest(){
+        PrintWriter newWriter = new PrintWriter(new FileWriter(fileRead));
+        Assert.assertTrue(MatrixServices.writeToFile(array, newWriter));
     }
 
     @Test
-    public void readNewMatrixTest() {
-        MatrixServices mt = new MatrixServices();
-        Assert.assertNotEquals(mt.newMatrixArray(), null);
+    public void createMatrixTest(){
+        Assert.assertNotEquals(MatrixServices.createMatrix(array), null);
     }
 
     @Test
-    public void readFromFileTest() {
-        MatrixServices mt = new MatrixServices();
-        Assert.assertNotEquals(mt.readFromFile(), null);
+    public void matrixToStringTest(){
+        Assert.assertNotEquals(MatrixServices.printStringMatrix(array), null);
     }
 }
